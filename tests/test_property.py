47446da1b1f899b275ac28603d75e007c73e1ce2
Didier DUMET <didier.dumet@gmail.com>
2021-04-11 11:46:07 +0200

    Add tests for creating a property

--- tests/test_property.py
+++ tests/test_property.py
@@ -1,11 +1,35 @@
 from pyngsild.property import Property
+from datetime import datetime
+import pytz
+
+timezone_France = pytz.timezone('Europe/Paris')
+PROP_NAME = 'myproperty'
+PROP_VALUE = 17
+PROP_OBSERVED_AT = timezone_France.localize(datetime.now()).isoformat()
+PROP_UNIT_CODE = 'SEC'
+PROP_DATASETID = 'urn:mydatasetid:1'
 
 
-def test_create_name_value_as_str():
-    p = Property('name', '3')
-    assert p._name == 'name' and isinstance(p._value, str)
+def test_create_name_value():
+    p = Property(PROP_NAME, PROP_VALUE)
+    assert p.name == PROP_NAME and p.value == PROP_VALUE
 
 
-def test_create_name_value_as_number():
-    p = Property('name', 3)
-    assert p._name == 'name' and isinstance(p._value, (int, float))
+def test_create_name_value_observed_at():
+    p = Property(PROP_NAME, PROP_VALUE, PROP_OBSERVED_AT)
+    assert p.name == PROP_NAME and p.value == PROP_VALUE \
+        and p.observed_at == PROP_OBSERVED_AT
+
+
+def test_create_name_value_observed_unit_code():
+    p = Property(PROP_NAME, PROP_VALUE, PROP_OBSERVED_AT, PROP_UNIT_CODE)
+    assert p.name == PROP_NAME and p.value == PROP_VALUE \
+        and p.observed_at == PROP_OBSERVED_AT and p.unit_code == PROP_UNIT_CODE
+
+
+def test_create_all_args():
+    p = Property(PROP_NAME, PROP_VALUE, PROP_OBSERVED_AT,
+                 PROP_UNIT_CODE, PROP_DATASETID)
+    assert p.name == PROP_NAME and p.value == PROP_VALUE \
+        and p.observed_at == PROP_OBSERVED_AT \
+        and p.unit_code == PROP_UNIT_CODE and p._datasetid == PROP_DATASETID
