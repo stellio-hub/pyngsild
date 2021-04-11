95026f39c0bf551c9bddf378bb2f108a0eebd847
Didier DUMET <didier.dumet@gmail.com>
2021-04-11 18:07:07 +0200

    add tests on generating NGSI-LD of properties

--- tests/test_property.py
+++ tests/test_property.py
@@ -10,6 +10,17 @@ PROP_UNIT_CODE = 'SEC'
 PROP_DATASETID = 'urn:mydatasetid:1'
 
 
+# Some utilities functions
+def get_a_property():
+    return(Property(name='plant_health',
+                    value=5, observed_at=PROP_OBSERVED_AT))
+
+
+def get_a_sub_property():
+    return(Property(name='temperature',
+                    value=37, observed_at=PROP_OBSERVED_AT, unit_code='CEL'))
+
+
 def test_create_name_value():
     p = Property(PROP_NAME, PROP_VALUE)
     assert p.name == PROP_NAME and p.value == PROP_VALUE
@@ -33,3 +44,35 @@ def test_create_all_args():
     assert p.name == PROP_NAME and p.value == PROP_VALUE \
         and p.observed_at == PROP_OBSERVED_AT \
         and p.unit_code == PROP_UNIT_CODE and p._datasetid == PROP_DATASETID
+
+
+def test_to_ngsild_one_property():
+    p = get_a_property()
+    ngsild_true = {
+        'plant_health': {
+            'type': 'Property',
+            'value': 5,
+            'observedAt': PROP_OBSERVED_AT
+        }
+    }
+    assert p.to_ngsild() == ngsild_true
+
+
+def test_to_ngsild_one_sub_property():
+    p = get_a_property()
+    sub_p = get_a_sub_property()
+    p.add_properties(sub_p)
+    ngsild_true = {
+        'plant_health': {
+            'type': 'Property',
+            'value': 5,
+            'observedAt': PROP_OBSERVED_AT,
+            'temperature': {
+                'type': 'Property',
+                'value': 37,
+                'observedAt': PROP_OBSERVED_AT,
+                'unitCode': 'CEL'
+            }
+        }
+    }
+    assert p.to_ngsild() == ngsild_true
