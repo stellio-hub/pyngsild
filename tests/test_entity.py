4724f06840d4dd8cb3abd1bd14e7db2fface0486
Didier DUMET <didier.dumet@gmail.com>
2021-04-13 16:38:49 +0200

    update tests to use global test vars

--- tests/test_entity.py
+++ tests/test_entity.py
@@ -1,82 +1,58 @@
 from pyngsild.entity import Entity
-from pyngsild.property import Property
-from datetime import datetime
-import pytz
-
-timezone_France = pytz.timezone('Europe/Paris')
-
-ID = 'uri:myentity:1'
-TYPE = 'MY_ENTITY'
-
-AT_CONTEXT = [
-    'https://raw.githubusercontent.com/dummy/ngsild-api-data-models/'
-    'main/jsonld-contexts/mydummy-contexts.jsonld'
-]
-AT_CONTEXT_LINK = '<https://raw.githubusercontent.com/senseen/'\
-    'ngsild-api-data-models/main/scanner/jsonld-contexts/'\
-    'scanSmartMeter-compound.jsonld>; '\
-    'rel=http://www.w3.org/ns/json-ld#context; type=application/json'
-
-PROP_OBSERVED_AT = timezone_France.localize(datetime.now()).isoformat()
-PROP_1 = Property(name='plant_health', value=5,
-                  observed_at=PROP_OBSERVED_AT)
-PROP_2 = Property(name='temperature', value=37,
-                  observed_at=PROP_OBSERVED_AT, unit_code='CEL')
-PROP_3 = Property(name='pH', value=7.3, observed_at=PROP_OBSERVED_AT,
-                  unit_code='C62')
+from . import global_test_vars as g
 
 
 # Some utilities functions
 def get_an_entity():
-    e = Entity(id=ID, type=TYPE)
+    e = Entity(id=g.ENT_ID, type=g.ENT_TYPE)
     return(e)
 
 
 def get_an_entity_with_context():
     e = get_an_entity()
-    e.at_context = AT_CONTEXT
+    e.at_context = g.AT_CONTEXT
     return(e)
 
 
 # TESTS
 def test_create_id_type():
-    e = Entity(ID, TYPE)
-    assert e.id == ID and e.type == TYPE
+    e = get_an_entity()
+    assert e.id == g.ENT_ID and e.type == g.ENT_TYPE
 
 
 def test_set_context():
-    e = Entity(ID, TYPE)
-    e.at_context = AT_CONTEXT
-    assert e.at_context == AT_CONTEXT
+    e = get_an_entity()
+    e.at_context = g.AT_CONTEXT
+    assert e.at_context == g.AT_CONTEXT
 
 
 def test_set_one_property():
     e = get_an_entity()
-    e.properties = PROP_1
+    e.properties = g.PROP_1
     # when set, a single property is set as a  list
     # hence assertion to [PROP_1]
-    assert e.properties == [PROP_1]
+    assert e.properties == [g.PROP_1]
 
 
 def test_add_first_property():
     e = get_an_entity()
-    e.add_properties(PROP_1)
+    e.add_properties(g.PROP_1)
     # when added, a single property is added as a list
     # hence assertion to [PROP_1]
-    assert e.properties == [PROP_1]
+    assert e.properties == [g.PROP_1]
 
 
 def test_add_another_property():
     e = get_an_entity()
-    e.add_properties(PROP_1)
-    e.add_properties(PROP_2)
-    assert e.properties == [PROP_1, PROP_2]
+    e.add_properties(g.PROP_1)
+    e.add_properties(g.PROP_2)
+    assert e.properties == [g.PROP_1, g.PROP_2]
 
 
 def test_add_list_of_properties():
     e = get_an_entity()
-    e.add_properties([PROP_1, PROP_2])
-    assert e.properties == [PROP_1, PROP_2]
+    e.add_properties([g.PROP_1, g.PROP_2])
+    assert e.properties == [g.PROP_1, g.PROP_2]
 
 
 def test_simple_entity_to_ngsild():
@@ -104,7 +80,7 @@ def test_simple_entity_with_context_to_ngsild():
 
 def test_entity_two_properties_to_nsgild():
     e = get_an_entity_with_context()
-    e.add_properties([PROP_1, PROP_2])
+    e.add_properties([g.PROP_1, g.PROP_2])
     ngsild_true = {
         '@context': [
             'https://raw.githubusercontent.com/dummy/ngsild-api-data-models/'
@@ -115,12 +91,12 @@ def test_entity_two_properties_to_nsgild():
         'plant_health': {
             'type': 'Property',
             'value': 5,
-            'observedAt': PROP_OBSERVED_AT
+            'observedAt': g.OBSERVED_AT
         },
         'temperature': {
             'type': 'Property',
             'value': 37,
-            'observedAt': PROP_OBSERVED_AT,
+            'observedAt': g.OBSERVED_AT,
             'unitCode': 'CEL'
         }
     }
@@ -129,8 +105,8 @@ def test_entity_two_properties_to_nsgild():
 
 def test_entity_one_property_one_sub_property_to_nsgild():
     e = get_an_entity_with_context()
-    p = PROP_1
-    p.add_properties(PROP_2)
+    p = g.PROP_1
+    p.add_properties(g.PROP_2)
     e.add_properties(p)
     ngsild_true = {
         '@context': [
@@ -142,11 +118,11 @@ def test_entity_one_property_one_sub_property_to_nsgild():
         'plant_health': {
             'type': 'Property',
             'value': 5,
-            'observedAt': PROP_OBSERVED_AT,
+            'observedAt': g.OBSERVED_AT,
             'temperature': {
                 'type': 'Property',
                 'value': 37,
-                'observedAt': PROP_OBSERVED_AT,
+                'observedAt': g.OBSERVED_AT,
                 'unitCode': 'CEL'
             }
         }
