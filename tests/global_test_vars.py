eabde020a9aea7eecc34592d4c6efef536f0d5e8
Didier DUMET <didier.dumet@gmail.com>
2021-04-13 16:36:40 +0200

    add a module with global vars for all tests

---
+++ tests/global_test_vars.py
@@ -0,0 +1,34 @@
+from pyngsild.property import Property
+from pyngsild.relationship import Relationship
+from datetime import datetime
+import pytz
+
+timezone_France = pytz.timezone('Europe/Paris')
+OBSERVED_AT = timezone_France.localize(datetime.now()).isoformat()
+
+
+# CONTEXT
+AT_CONTEXT = [
+    'https://raw.githubusercontent.com/dummy/ngsild-api-data-models/'
+    'main/jsonld-contexts/mydummy-contexts.jsonld'
+]
+AT_CONTEXT_LINK = '<https://raw.githubusercontent.com/senseen/'\
+    'ngsild-api-data-models/main/scanner/jsonld-contexts/'\
+    'scanSmartMeter-compound.jsonld>; '\
+    'rel=http://www.w3.org/ns/json-ld#context; type=application/json'
+
+# GLOBALS for Entity Class tests
+ENT_ID = 'uri:myentity:1'
+ENT_TYPE = 'MY_ENTITY'
+
+# GLOBALS for Property Class tests
+PROP_1 = Property(name='plant_health', value=5,
+                  observed_at=OBSERVED_AT)
+PROP_2 = Property(name='temperature', value=37,
+                  observed_at=OBSERVED_AT, unit_code='CEL')
+PROP_3 = Property(name='pH', value=7.3, observed_at=OBSERVED_AT,
+                  unit_code='C62')
+
+# GLOBALS for Relationship tests
+R_NAME = 'to_object_1'
+R_OBJECT = 'uri:object_1'
