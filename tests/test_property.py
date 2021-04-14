51cfb81f126d574fe3d6c413e07a729de2816521
Didier DUMET <didier.dumet@gmail.com>
2021-04-14 16:39:11 +0200

    change upgrade all tests to make use of test global vars

--- tests/test_property.py
+++ tests/test_property.py
@@ -1,79 +1,69 @@
 from pyngsild.property import Property
-from datetime import datetime
-import pytz
-
-timezone_France = pytz.timezone('Europe/Paris')
-PROP_NAME = 'myproperty'
-PROP_VALUE = 17
-PROP_OBSERVED_AT = timezone_France.localize(datetime.now()).isoformat()
-PROP_UNIT_CODE = 'SEC'
-PROP_DATASETID = 'urn:mydatasetid:1'
+from . import global_test_vars as g
 
 
 # Some utilities functions
 def get_a_property():
     return(Property(name='plant_health',
-                    value=5, observed_at=PROP_OBSERVED_AT))
+                    value=5, observed_at=g.OBSERVED_AT))
 
 
 def get_a_sub_property():
     return(Property(name='temperature',
-                    value=37, observed_at=PROP_OBSERVED_AT, unit_code='CEL'))
+                    value=37, observed_at=g.OBSERVED_AT, unit_code='CEL'))
 
 
 # TESTS
 def test_create_name_value():
-    p = Property(PROP_NAME, PROP_VALUE)
-    assert p.name == PROP_NAME and p.value == PROP_VALUE
+    p = Property(g.P_NAME, g.P_VALUE)
+    assert p.name == g.P_NAME and p.value == g.P_VALUE
 
 
 def test_create_name_value_observed_at():
-    p = Property(PROP_NAME, PROP_VALUE, PROP_OBSERVED_AT)
-    assert p.name == PROP_NAME and p.value == PROP_VALUE \
-        and p.observed_at == PROP_OBSERVED_AT
+    p = Property(g.P_NAME, g.P_VALUE, g.OBSERVED_AT)
+    assert p.name == g.P_NAME and p.value == g.P_VALUE \
+        and p.observed_at == g.OBSERVED_AT
 
 
 def test_create_name_value_observed_unit_code():
-    p = Property(PROP_NAME, PROP_VALUE, PROP_OBSERVED_AT, PROP_UNIT_CODE)
-    assert p.name == PROP_NAME and p.value == PROP_VALUE \
-        and p.observed_at == PROP_OBSERVED_AT and p.unit_code == PROP_UNIT_CODE
+    p = Property(g.P_NAME, g.P_VALUE, g.OBSERVED_AT, g.P_UNIT_CODE)
+    assert p.name == g.P_NAME and p.value == g.P_VALUE \
+        and p.observed_at == g.OBSERVED_AT and p.unit_code == g.P_UNIT_CODE
 
 
 def test_create_all_args():
-    p = Property(PROP_NAME, PROP_VALUE, PROP_OBSERVED_AT,
-                 PROP_UNIT_CODE, PROP_DATASETID)
-    assert p.name == PROP_NAME and p.value == PROP_VALUE \
-        and p.observed_at == PROP_OBSERVED_AT \
-        and p.unit_code == PROP_UNIT_CODE and p._datasetid == PROP_DATASETID
+    p = Property(g.P_NAME, g.P_VALUE, g.OBSERVED_AT,
+                 g.P_UNIT_CODE, g.P_DATASETID)
+    assert p.name == g.P_NAME and p.value == g.P_VALUE \
+        and p.observed_at == g.OBSERVED_AT \
+        and p.unit_code == g.P_UNIT_CODE and p._datasetid == g.P_DATASETID
 
 
 def test_to_ngsild_one_property():
-    p = get_a_property()
+    p = g.PROP_1
     ngsild_true = {
         'plant_health': {
             'type': 'Property',
             'value': 5,
-            'observedAt': PROP_OBSERVED_AT
+            'observed_at': g.OBSERVED_AT
         }
     }
     assert p.to_ngsild() == ngsild_true
 
 
 def test_to_ngsild_one_sub_property():
-    p = get_a_property()
-    sub_p = get_a_sub_property()
-    p.add_properties(sub_p)
+    g.PROP_1.add_properties(g.PROP_2)
     ngsild_true = {
         'plant_health': {
             'type': 'Property',
             'value': 5,
-            'observedAt': PROP_OBSERVED_AT,
+            'observed_at': g.OBSERVED_AT,
             'temperature': {
                 'type': 'Property',
                 'value': 37,
-                'observedAt': PROP_OBSERVED_AT,
+                'observed_at': g.OBSERVED_AT,
                 'unitCode': 'CEL'
             }
         }
     }
-    assert p.to_ngsild() == ngsild_true
+    assert g.PROP_1.to_ngsild() == ngsild_true
