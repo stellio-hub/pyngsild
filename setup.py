7e2722042bbcd044eecff23b6d033c83f3f78589
Didier DUMET <didier.dumet@gmail.com>
2021-09-29 07:45:07 +0200

    add install_requires for auto-install of required packages (#27)
    fix(install): add installation of required python packages

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyngsild",
-    version="0.5.0",
+    version="0.6.0",
     packages=find_packages(),
+    install_requires=[
+        'pytest==6.2.4',
+        'pytz==2021.1',
+        'tzlocal==2.1',
+        'pytest-mock-server==0.2.0',
+        'requests==2.25.1',
+    ],
 )
